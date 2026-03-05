80e840bcbd51af0b3e8d0887d9f6f327ec320c6a
RITESH BHOGEKAR <riteshbhogekar1234@gmail.com>
2026-03-05 11:53:07 +0530

    fix server frontend issue it  was not showing earlier

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 const urlRoutes = require("./routes/urlRoutes");
 
 const app = express();
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend")));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -28,7 +30,7 @@ mongoose
   });
 
 app.get("/", (req, res) => {
-  res.send("✅ URL Shortener API Running");
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 app.use("/", urlRoutes);
